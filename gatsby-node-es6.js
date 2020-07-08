fbb75472bbeca50b673681b92f57fb1c9b9ea0bb
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-08 22:42:39 +0700

    :construction: Add slug fields to graphql

--- gatsby-node-es6.js
+++ gatsby-node-es6.js
@@ -19,8 +19,14 @@ exports.createPages = ({ actions }) => {
 
 import { createFilePath } from "gatsby-source-filesystem"
 
-exports.onCreateNode = ({ node, getNode }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(createFilePath({ node, getNode, basePath: `pages` }))
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
 }
